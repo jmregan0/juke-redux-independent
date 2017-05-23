3e19d7b0076721ed0827fbc612ced8e241120970
Jacob Regan <jacobmregan0@gmail.com>
2017-05-23 08:28:55 -0500

    Moved Player action creators into their own file

--- browser/react/index.js
+++ browser/react/index.js
@@ -29,3 +29,37 @@ ReactDOM.render(
   </Router>,
   document.getElementById('app')
 );
+
+/*
+    React Flow:
+
+    1) Constraints (define action types)
+    2) Action Creators () =>
+    3) Reducer
+    4) Container (class)
+      -Subscribe to store
+      -Unsubscribe
+      -Dispatches actions when appropriate
+    5) Components (presentational/dumb)
+
+
+    ***************************************
+
+    Action type - Anonymous Func that returns an
+    object with an action type ({type: START_PLAY})
+
+    Action Creators - In Container, functions that fire on certain actions
+    which will be tracked on the dom with listeners that
+    are rendered by containers components.
+
+    Reducer - Function passed initial state and action.
+    Based on what the action is, it will build onto the newState
+    it is creating and attempt to update initial state with info.
+
+    Container - Main hub(class extention) that has constructor,
+    special functions, and renders components which contain HTML.
+
+    Components - Don't know how to interact with React etc. Just
+    contains the HTML for its container to eventually render to DOM.
+
+*/
